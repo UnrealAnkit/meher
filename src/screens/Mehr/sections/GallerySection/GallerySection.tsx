9204cc266ec4114f3ba765c7580082ae89bd8c98
UnrealAnkit <work.ankit2@gmail.com>
2025-11-13 20:10:53 +0530

    changes

--- src/screens/Mehr/sections/GallerySection/GallerySection.tsx
+++ src/screens/Mehr/sections/GallerySection/GallerySection.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export const GallerySection = (): JSX.Element => {
   return (
-    <section className="w-full flex flex-col py-2.5">
-      <h2 className="[font-family:'Poppins',Helvetica] font-semibold text-[#7a574f] text-2xl text-center tracking-[5.00px] leading-[20px] whitespace-nowrap mb-2">
+    <section className="w-full flex flex-col py-2.5 px-4">
+      <h2 className="[font-family:'Poppins',Helvetica] font-semibold text-[#7a574f] text-base sm:text-lg md:text-xl lg:text-2xl text-center tracking-[2px] sm:tracking-[3px] md:tracking-[4px] lg:tracking-[5px] leading-[20px] sm:leading-[24px] md:leading-[28px] mb-2">
         HEALING IN COMMUNITY
       </h2>
 
-      <p className="[font-family:'Poppins',Helvetica] font-medium text-[#7a574f4c] text-[42px] text-center tracking-[0] leading-[50px] whitespace-nowrap">
+      <p className="[font-family:'Poppins',Helvetica] font-medium text-[#7a574f4c] text-sm sm:text-base md:text-2xl lg:text-3xl xl:text-[42px] text-center tracking-[0] leading-[18px] sm:leading-[22px] md:leading-[32px] lg:leading-[42px] xl:leading-[50px] break-words">
         CORPORATE – GROUPS – FESTIVALS – INTERNATIONAL
       </p>
     </section>
